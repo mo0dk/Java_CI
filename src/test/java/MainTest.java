06b0bb0caccfd083b666a68e8623a65b3bf15707
Evgenii <37473276+mo0dk@users.noreply.github.com>
2024-06-04 16:25:36 +0300

    Create MainTest.java

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+    @Test
+    public void testMin() {
+        List<Integer> numbers = Arrays.asList(1, 4, 2, 3);
+        assertEquals(1, Main._min(numbers));
+    }
+
+    @Test
+    public void testMax() {
+        List<Integer> numbers = Arrays.asList(1, 4, 2, 3);
+        assertEquals(4, Main._max(numbers));
+    }
+
+    @Test
+    public void testSum() {
+        List<Integer> numbers = Arrays.asList(1, 4, 2, 3);
+        assertEquals(10, Main._sum(numbers));
+    }
+
+    @Test
+    public void testMult() {
+        List<Integer> numbers = Arrays.asList(1, 4, 2, 3);
+        assertEquals(24, Main._mult(numbers));
+    }
+}
